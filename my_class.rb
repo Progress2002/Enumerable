598744b3d06fc5101ee258a7227ccf49bcf664b0
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-05-11 13:52:27 +0000

    Import the module and add test cases

--- my_class.rb
+++ my_class.rb
@@ -1,4 +1,8 @@
+require_relative 'my_enumerable'
+
 class MyList
+  include MyEnumerable
+
   def initialize(*num)
     @list = num
   end
@@ -7,3 +11,17 @@ class MyList
     @list.each(&block)
   end
 end
+
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+list.all? {|e| e < 5}
+list.all? {|e| e > 5}
+
+# Test #any?
+list.any? {|e| e == 2}
+list.any? {|e| e == 5}
+
+# Test #filter
+list.filter {|e| e.even?}
+[2, 4]
